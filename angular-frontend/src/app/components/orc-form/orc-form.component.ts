68b40da8f6ebaa7d5bd8d25c51900ea0014fb7dd
BorbasMate <borbas.mategabor76@gmail.com>
2023-11-27 16:02:22 +0100

    add saveOrc

--- angular-frontend/src/app/components/orc-form/orc-form.component.ts
+++ angular-frontend/src/app/components/orc-form/orc-form.component.ts
@@ -4,11 +4,12 @@ import {AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validat
 import {OrcService} from '../../services/orc.service';
 import {RaceTypeOptionModel} from '../../models/raceTypeOption.model';
 import {WeaponOptionModel} from '../../models/weaponOption.model';
+import {OrcFormDataModel} from "../../models/orcFormData.model";
 
 @Component({
-             selector   : 'app-orc-form',
-             templateUrl: './orc-form.component.html',
-           })
+  selector: 'app-orc-form',
+  templateUrl: './orc-form.component.html',
+})
 export class OrcFormComponent implements OnInit {
 
   formData: FormGroup;
@@ -18,10 +19,10 @@ export class OrcFormComponent implements OnInit {
 
   constructor(private orcService: OrcService, private formBuilder: FormBuilder) {
     this.formData = this.formBuilder.group({
-      name   : ['', Validators.required],
-      raceType : ['', Validators.required],
-      killCount : [null, [Validators.required, Validators.min(0)]],
-      weapons: this.formBuilder.array([],this.checkBoxValidator.bind(this)),
+      name: ['', Validators.required],
+      raceType: ['', Validators.required],
+      killCount: [null, [Validators.required, Validators.min(0)]],
+      weapons: this.formBuilder.array([], this.checkBoxValidator.bind(this)),
     });
   }
 
@@ -31,33 +32,48 @@ export class OrcFormComponent implements OnInit {
         this.weaponOptions = formInitData.weapons;
         this.createCheckboxControls();
         this.raceTypes = formInitData.raceTypes;
+
+        // console.log(this.weaponOptions);
+        // console.log(this.raceTypes);
+
       });
-    }
+  }
 
 
-    private createCheckboxControls() {
-        this.weaponOptions.forEach(() => {
-            const control = new FormControl(false);
-            (<FormArray>this.formData.controls.weapons).push(control);
-        });
-    }
+  private createCheckboxControls() {
+    this.weaponOptions.forEach(() => {
+      const control = new FormControl(false);
+      (<FormArray>this.formData.controls.weapons).push(control);
+    });
+  }
 
-    private createWeaponsArrayToSend(): string[] {
-      return this.formData.value.weapons
-                 .map((weapon: string, index: number) => weapon ? this.weaponOptions[index].name : null)
-                 .filter((weapon: string) => weapon !== null);
-    }
+  private createWeaponsArrayToSend(): string[] {
+    return this.formData.value.weapons
+      .map((weapon: string, index: number) => weapon ? this.weaponOptions[index].name : null)
+      .filter((weapon: string) => weapon !== null);
+  }
 
-    saveOrc() {
-        const data = {...this.formData.value};
-        data.weapons = this.createWeaponsArrayToSend();
-    }
+  saveOrc() {
 
-    checkBoxValidator(arr: AbstractControl): { required: boolean } {
-        let counter = 0;
-      if (arr instanceof FormArray) {
-        arr.getRawValue().forEach(value => value ? counter++ : counter);
-      }
-        return counter > 3 ? {required: true} : null;
+    const data: OrcFormDataModel = {...this.formData.value};
+    data.weapons = this.createWeaponsArrayToSend();
+
+    console.log(data);
+
+    this.orcService.createOrc(data).subscribe({
+      next: (value) => console.log('Sikeres mentés!'),
+      error: (err) => console.log(err),
+      complete: () => console.log('Lefutott a complete ág!')
+    });
+
+
+  }
+
+  checkBoxValidator(arr: AbstractControl): { required: boolean } {
+    let counter = 0;
+    if (arr instanceof FormArray) {
+      arr.getRawValue().forEach(value => value ? counter++ : counter);
     }
+    return counter > 3 ? {required: true} : null;
+  }
 }
